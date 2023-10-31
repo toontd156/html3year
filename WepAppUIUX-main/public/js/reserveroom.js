7975c5e9b30a0ef62e8e6f82991cebffabd4399c
Watcharin <76784110+toontd156@users.noreply.github.com>
2023-11-01 02:40:43 +0700

    หฟฟฟ

---
+++ WepAppUIUX-main/public/js/reserveroom.js
@@ -0,0 +1,25 @@
+const checkdate = document.querySelector('#checkdate');
+const selectroom = document.querySelector('#selectroom');
+const selecttime = document.querySelector('#selecttime');
+const timecount = document.querySelector('#timecount');
+const countpeople = document.querySelector('#countpeople');
+const itemaddon = document.querySelector('#itemaddon');
+const formsubmitdetail = document.querySelector('#submit-detail');
+
+formsubmitdetail.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (checkdate.value == "" || selectroom.value == "" || selecttime.value == "" || timecount.value == "" || countpeople.value == "") {
+        alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+        return;
+    } else{
+        window.location.assign('confirm_reserveroom.html')
+    }
+    const data = {
+        checkdate: checkdate.value,
+        selectroom: selectroom.value,
+        selecttime: selecttime.value,
+        timecount: timecount.value,
+        countpeople: countpeople.value,
+        itemaddon: itemaddon.value
+    }
+})
